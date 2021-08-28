01cbba68cc26df5715a536026c3a4d931361e7f8
5ives <ivesparas@gmail.com>
2021-08-28 16:32:52 +1000

    added login button

--- spotify-stats/src/App.js
+++ spotify-stats/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Home from './pages/Home';
 
 function App() {
   	return (
 		<div className="App">
 			<div className="App-header">
-				Spotify Stats
+				<Home/>
 			</div>
 		</div>
 	);
