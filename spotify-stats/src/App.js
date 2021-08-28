c43378172564628be2e3cfac7493a423e38580b3
5ives <ivesparas@gmail.com>
2021-08-28 14:37:01 +1000

    initialised website

---
+++ spotify-stats/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  	return (
+		<div className="App">
+			<div className="App-header">
+				Spotify Stats
+			</div>
+		</div>
+	);
+}
+
+export default App;
