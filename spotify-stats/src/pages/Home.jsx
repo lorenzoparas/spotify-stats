33e454a4fd4a3768c5ff5b759ad831fbef732450
Aryan <karwalaryan@gmail.com>
2021-08-29 13:11:25 +1000

    shifting branches

--- spotify-stats/src/pages/Home.jsx
+++ spotify-stats/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import "./Home.css"
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 const clientId = '33c58056810e472dbde6aa97618d59c9';
 const redirectUri = 'https%3A%2F%2Fexample.com%2F';
@@ -8,6 +10,22 @@ const fetchUrl = 'https://accounts.spotify.com/authorize' +
     '&response_type=code' +
     '&redirect_uri=' + redirectUri;
 
+    
+const StyledButton = withStyles({
+    root: {
+      background: '#62CD32',
+      borderRadius: '20px',
+      border: 0,
+      color: 'black',
+      padding: '0 30px',
+      width: '95px',
+      height: '35px',
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
+
 function Home() {
 
     const handleLogin = () => {
@@ -15,12 +33,20 @@ function Home() {
     };
 
     return (
-        <div>
-            Hey Shitheadd welcome to Spotify Stats
-            <div>
-                <Button variant="contained" color="primary" onClick={handleLogin}>
-                LOGIN
-                </Button>
+        <div id="main">
+            <span className="title" id="spot1">
+                Spotify 
+            </span>
+            <span className="title">
+                Stats
+            </span>
+            <div id="appInfo">
+                Spotify Stats presents to you an easy way of monitoring and accessing your spotify activity. Find which songs, artists and albums you listen to the most and how your taste in music has evolved. 
+                Just login with your Spotify account to get started.
+            </div>
+            
+            <div id="loginButton">
+                <StyledButton onClick={handleLogin}> LOGIN </StyledButton>
             </div>
         </div>
     );
